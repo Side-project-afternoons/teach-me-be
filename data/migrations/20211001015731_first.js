864d99c161b906c27345a45563e013c1ab1c5b2e
francis-nguyen92 <francishtknguyen@gmail.com>
2021-10-21 16:49:07 -0500

    user routes and migration complete

--- data/migrations/20211001015731_first.js
+++ data/migrations/20211001015731_first.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex) {
+exports.up = async function (knex) {
+    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    return knex.schema.createTable("users", (tbl) => {
+      tbl
+        .uuid("user_id")
+        .notNullable()
+        .primary()
+        .defaultTo(knex.raw("uuid_generate_v4()"));
+      tbl.string("username", 128).notNullable().unique();
+      tbl.string("password", 128).notNullable();
+    });
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = async function (knex) {
+    return knex.schema.dropTableIfExists("users");
+  };
