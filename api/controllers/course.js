91e92ca07d9464cba1efb53caa208160bd721c09
francis-nguyen92 <francishtknguyen@gmail.com>
2021-10-22 16:04:08 -0500

    fixed status codes and added routes

--- api/controllers/course.js
+++ api/controllers/course.js
@@ -14,19 +14,19 @@ const courseController = {
         res.status(200).json(await Courses.getUserCourseById(req.params.user_id))
     },
     async addCourse(req,res) {
-        res.status(200).json(await Courses.addCourse(req.body))
+        res.status(201).json(await Courses.addCourse(req.body))
     },
     async addCourseToUser(req,res) {
-        res.status(200).json(await Courses.addUserCourse(req.body))
+        res.status(201).json(await Courses.addUserCourse(req.body))
     },
     async editCourse(req,res) {
-        res.status(200).json(await Courses.editCourse(req.body))
+        res.status(201).json(await Courses.editCourse(req.body))
     },
     async deleteUserCourse(req,res) {
-        res.status(200).json(await Courses.deleteUserCourse(req.body))
+        res.status(202).json(await Courses.deleteUserCourse(req.body))
     },
     async deleteCourse(req,res) {
-        res.status(200).json(await Courses.deleteUserCourse(req.body))
+        res.status(202).json(await Courses.deleteUserCourse(req.body))
     }
 };
 
--- api/routes/course.js
+++ api/routes/course.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 const courseController = require('../controllers/course')
 
-router.get("/", courseController)
+router.get("/", courseController.getAll)
+router.get("/:course_id", courseController.getCourseById)
+router.get("/owner/:user_id", courseController.getCourseByOwnerId)
+router.get("/user/:user_id", courseController.getUserCourseById)
+router.get("/owner", courseController.addCourse)
+router.get("/user", courseController.addCourseToUser)
+router.get("/", courseController.editCourse)
+router.get("/owner", courseController.deleteCourse)
+router.get("/user", courseController.deleteUserCourse)
 module.exports = router
